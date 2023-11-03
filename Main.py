909de85ae8d57e72cd1abd782bf08199cf91bb16
Brandon Sicay <sicayb@gmail.com~>
2023-11-02 22:13:12 -0600

    fianl render

---
+++ Main.py
@@ -0,0 +1,58 @@
+
+import pygame
+from pygame.locals import *
+import glm
+
+from gl import Renderer
+from model import Model
+from shaders import *
+
+width = 960
+height = 540
+
+pygame.init()
+
+screen = pygame.display.set_mode((width,height),pygame.OPENGL|pygame.DOUBLEBUF)
+clock = pygame.time.Clock()
+
+rend = Renderer(screen)
+rend.setShaders(vertex_shader,fragment_shader)
+
+modelo = Model(filename="models/Charizard.obj",translate=glm.vec3(0,-2,-5),rotation=glm.vec3(0,0,0),scale=glm.vec3(0.2,0.2,0.2))
+modelo.loadTexture("textures/charizard.bmp")
+
+rend.scene.append(modelo)
+
+isRunning = True
+while isRunning:
+    deltaTime = clock.tick(60)/1000
+    
+    keys = pygame.key.get_pressed()
+    
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            isRunning = False
+            
+        elif event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                isRunning = False
+    
+    if keys[K_d]:
+        rend.camPosition.x += 5 * deltaTime #5 unidades por segundo
+    elif keys[K_a]:
+        rend.camPosition.x -= 5 * deltaTime
+        
+    if keys[K_w]:
+        rend.camPosition.z -= 5 * deltaTime
+    elif keys[K_s]:
+        rend.camPosition.z += 5 * deltaTime
+        
+    if keys[K_g]:
+        rend.camPosition.y += 5 * deltaTime
+    elif keys[K_e]:
+        rend.camPosition.y -= 5 * deltaTime
+    
+    rend.render()
+    pygame.display.flip()
+    
+pygame.quit()
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# OpenGL
+
+
+#  OGL1: 3D Models & Transforms 🖥️
+
+
+## Screenshot 
+![enter image description here](https://cdn.discordapp.com/attachments/717418780065529856/1178704266316238888/image.png?ex=65771ce4&is=6564a7e4&hm=5e72fb426f9f3f907ba901343b80fc41248f23d67369725364817d216927af43&)
